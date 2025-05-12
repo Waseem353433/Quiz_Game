a9861a1023125c7e4dab04ef0c7291559710d265
Waseem Ahmad <waseemjan3534@gmail.com>
2025-05-13 00:49:53 +0500

    First two class TitleScreenGUI and App

---
+++ QuizGameClasses/TitleScreenGui.java
@@ -0,0 +1,21 @@
+package QuizGameClasses;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class TitleScreenGui extends JFrame {
+  public TitleScreenGui(){
+    super("Title Screen");
+    setSize(400,565);
+    setLayout(null);
+    setLocationRelativeTo(null);
+    setResizable(false);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+
+  }
+
+  }
