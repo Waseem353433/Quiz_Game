1db91318ad200a9a520d1a2bd40e9e19fd6d5342
Waseem Ahmad <waseemjan3534@gmail.com>
2025-05-20 00:54:31 +0500

    Add files via upload

---
+++ QuizQuestion.java
@@ -0,0 +1,34 @@
+package ClassesOfProject.quiz_question;
+
+public class QuizQuestion {
+    private int id;
+    private String question;
+    private String[] options;
+    private String answer;
+
+    public QuizQuestion(int id, String question, String[] options, String answer) {
+        this.id = id;
+        this.question = question;
+        this.options = options;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
+
+
+
